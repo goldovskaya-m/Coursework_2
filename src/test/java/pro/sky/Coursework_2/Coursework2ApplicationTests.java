a76dff83200f66b88b7d9ecaf4698b722b56b354
goldovskaya-m <ariya-aaa@yandex.ru>
2024-10-27 02:22:27 +0300

    Initial commit

---
+++ src/test/java/pro/sky/Coursework_2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Coursework_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
